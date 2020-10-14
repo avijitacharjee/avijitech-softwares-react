5f0a146cbcfd830bb172873b11f8317f8974f1ad
Avijit Acharjee <avijitach@gmail.com>
2020-10-14 19:00:18 +0600

    Updates

---
+++ precache-manifest.d4b83e07e9fb28f47897e24eb7a7864c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63567d70459d2cace02b827d945181de",
+    "url": "/avijitech-softwares-react/index.html"
+  },
+  {
+    "revision": "2d50c2592b502863ae12",
+    "url": "/avijitech-softwares-react/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "1c91d55108bc6ce92c9f",
+    "url": "/avijitech-softwares-react/static/js/2.dd8253b6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/avijitech-softwares-react/static/js/2.dd8253b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d50c2592b502863ae12",
+    "url": "/avijitech-softwares-react/static/js/main.b24fe4ad.chunk.js"
+  },
+  {
+    "revision": "716b92cf06859dc73dd0",
+    "url": "/avijitech-softwares-react/static/js/runtime-main.3c6c4e22.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/avijitech-softwares-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
